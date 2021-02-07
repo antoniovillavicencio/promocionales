420632b1f817c9ba3a599135d75ef1cba61c5367
Antonio Villavicencio <tonyvcj@gmail.com>
2021-02-07 10:54:10 -0600

    Add menu links to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,30 @@
 import React from "react"
-import styled from 'styled-components';
+import styled from "styled-components"
 import { Link } from "gatsby"
-import {FaBars} from 'react-icons/fa';
+import { FaBars } from "react-icons/fa"
+import { menuData } from "../data/MenuData"
+import { Button } from "./Button"
+
+const Header = () => {
+
+  return (
+    <Nav>
+      <NavLink to="/">Explorix</NavLink>
+      <Bars />
+      <NavMenu>
+        {menuData.map((ref, index) => (
+          <NavLink to={ref.link} key={index}>
+            {ref.title}
+          </NavLink>
+        ))}
+      </NavMenu>
+      <NavBtn>
+        <Button primary round to={"/trips"}>Book a Flight</Button>
+      </NavBtn>
+    </Nav>
+  )
+}
+export default Header
 
 const Nav = styled.nav`
   background: red;
@@ -23,29 +46,37 @@ const NavLink = styled(Link)`
   cursor: pointer;
 `
 
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: -48px;
+  
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`
+
 const Bars = styled(FaBars)`
   display: none;
   color: #fff;
-  
+
   @media screen and (max-width: 768px) {
     display: block;
     position: absolute;
     top: 0;
     right: 0;
     transform: translate(-100%, 75%);
-    font-size: 1rem;
+    font-size: 1.8rem;
     cursor: pointer;
   }
 `
 
-const Header = () => {
-  return (
-    <Nav>
-      <NavLink to="/">Explorix</NavLink>
-      <Bars />
-    </Nav>
-  )
-}
-
-
-export default Header
+const NavBtn = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+  
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`
