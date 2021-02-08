0445942d851806655815ff4b330bfe846ad0565c
Antonio Villavicencio <tonyvcj@gmail.com>
2021-02-08 08:10:43 -0600

    Add Products section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/Hero"
+import Products from "../components/Products"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
+    <Products heading="Our Products"/>
   </Layout>
 )
 
